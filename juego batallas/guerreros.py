7b6dd606c1b4c0f87c16c05512d578065c42b7b1
jeca514 <jeca514@hotmail.com>
2025-01-26 10:40:46 -0500

    Mejorar la clase Guerrero: actualizar inicialización de raza y agregar validaciones; añadir método para calcular peso de victoria

--- juego batallas/guerreros.py
+++ juego batallas/guerreros.py
@@ -1,8 +1,20 @@
+import random
 
 class Guerrero:
-    def __init__(self, faccion, raza):
+    def __init__(self, faccion, clave_raza):  # Recibe facción y clave
         self._faccion = faccion
-        self._raza = raza
+        
+        # Obtiene la raza según la facción y clave
+        if faccion == "benevola":
+            self._raza = self.faccion_benevola.get(clave_raza)
+        elif faccion == "malvada":
+            self._raza = self.faccion_malvada.get(clave_raza)
+        else:
+            raise ValueError("Facción inválida")
+        
+        # Valida que la raza exista
+        if self._raza is None:
+            raise ValueError(f"Clave {clave_raza} no existe en la facción {faccion}")
 
     faccion_benevola = {
         1: "Ositos",
@@ -28,5 +40,38 @@ class Guerrero:
         "Fulos": {"Lolos": 1, "Fulanos": 0.5, "Hoggis": 2, "Lurcos": 3, "Trollis": 0.7},
     }
     
+    def generador
+    
+    def peso_victoria(self, enemigo):
+        # if que determina facción 
+        if self._faccion == "benevola":
+            peso_victoria = self.victoria_benevola[self._raza][enemigo._raza]
+            return peso_victoria
+        else:
+            peso_victoria = 1/self.victoria_benevola[enemigo._raza][self._raza]
+            return peso_victoria
+        
+    
+class Batallon:
+    def __init__(self, faccion):
+        self.faccion = faccion
+        self.guerreros = []  # Lista de objetos Guerrero
 
+    def agregar_guerrero(self, clave_raza):
+        nuevo_guerrero = Guerrero(faccion=self.faccion, clave_raza=clave_raza)
+        self.guerreros.append(nuevo_guerrero)
 
+    def generar_aleatorio(self, cantidad):
+        for _ in range(cantidad):
+            clave_random = random.randint(1, 5)  # Asume 5 razas por facción
+            self.agregar_guerrero(clave_random)
+
+    def calcular_fuerza(self, enemigo):
+         # if que determina facción 
+        if self._faccion == "benevola":
+            peso_victoria = self.victoria_benevola[self._raza][enemigo._raza]
+            return peso_victoria
+        else:
+            peso_victoria = 1/self.victoria_benevola[enemigo._raza][self._raza]
+            return peso_victoria
+       
